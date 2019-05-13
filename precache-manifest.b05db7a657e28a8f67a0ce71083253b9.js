1b11dd482e84239767ccb1e6e3ab313330bd4f4f
LexFlame <lex_flame@mail.ua>
2019-05-13 16:51:11 +0300

    Updates

---
+++ precache-manifest.b05db7a657e28a8f67a0ce71083253b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25675f35920cb8acf3ee588a6282a02b",
+    "url": "/react-art-app/index.html"
+  },
+  {
+    "revision": "0f1f12cf1b2b3bec6004",
+    "url": "/react-art-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-art-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0f1f12cf1b2b3bec6004",
+    "url": "/react-art-app/static/js/main.70e42f07.chunk.js"
+  },
+  {
+    "revision": "9603c63042f7801abe6f",
+    "url": "/react-art-app/static/js/runtime~main.de7c5b8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-art-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
